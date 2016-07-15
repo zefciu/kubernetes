34a6578dd526c70e9057d46a6314d35548d54b17
Daniel Smith <dbsmith@google.com>
2016-07-14 17:47:46 -0700

    Revert "Remove pod mutation for PVs with supplemental GIDs"

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -404,20 +404,6 @@ func NewMainKubelet(
 	klet.podCache = kubecontainer.NewCache()
 	klet.podManager = kubepod.NewBasicPodManager(kubepod.NewBasicMirrorClient(klet.kubeClient))
 
-	klet.volumePluginMgr, err =
-		NewInitializedVolumePluginMgr(klet, volumePlugins)
-	if err != nil {
-		return nil, err
-	}
-
-	klet.volumeManager, err = volumemanager.NewVolumeManager(
-		enableControllerAttachDetach,
-		hostname,
-		klet.podManager,
-		klet.kubeClient,
-		klet.volumePluginMgr,
-		klet.containerRuntime)
-
 	// Initialize the runtime.
 	switch containerRuntime {
 	case "docker":
@@ -435,7 +421,6 @@ func NewMainKubelet(
 			containerLogsDir,
 			osInterface,
 			klet.networkPlugin,
-			klet.volumeManager,
 			klet,
 			klet.httpClient,
 			dockerExecHandler,
@@ -511,6 +496,20 @@ func NewMainKubelet(
 		klet.runner,
 		containerRefManager,
 		recorder)
+
+	klet.volumePluginMgr, err =
+		NewInitializedVolumePluginMgr(klet, volumePlugins)
+	if err != nil {
+		return nil, err
+	}
+
+	klet.volumeManager, err = volumemanager.NewVolumeManager(
+		enableControllerAttachDetach,
+		hostname,
+		klet.podManager,
+		klet.kubeClient,
+		klet.volumePluginMgr,
+		klet.containerRuntime)
 
 	runtimeCache, err := kubecontainer.NewRuntimeCache(klet.containerRuntime)
 	if err != nil {
